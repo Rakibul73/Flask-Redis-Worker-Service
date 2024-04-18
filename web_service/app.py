0c0ccaf30f24151305386af799bc081881b09e67
Rakibul73 <rakibul29185@yahoo.com>
2024-04-19 02:42:30 +0600

    added a result route to see the updated data from redis

--- web_service/app.py
+++ web_service/app.py
@@ -22,14 +22,24 @@ def receive_task():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
+# endpoint to retrieve processed results
+@app.route('/results', methods=['GET'])
+def get_results():
+    try:
+        # Retrieve processed results from Redis
+        results = redis_client.lrange('processed_data', 0, -1)
+        # Convert bytes to strings
+        results_str = [json.loads(result) for result in results]
+        return jsonify({"results": results_str}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/monitor_queue', methods=['GET'])
 def monitor_queue():
     # Get the length of the queue
     queue_length = redis_client.llen('task_queue')
     # Get the list of tasks in the queue
-    tasks = redis_client.lrange('task_queue', 0, queue_length)
+    tasks = redis_client.lrange('task_queue', 0, -1)
     # Convert bytes to strings
     tasks_str = [task.decode('utf-8') for task in tasks]
     # Return the queue length and list of tasks as JSON
