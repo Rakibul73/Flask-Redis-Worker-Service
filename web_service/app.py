713c72b2565011d008b28eec82fde24f1e960e78
Rakibul73 <rakibul29185@yahoo.com>
2024-04-19 03:15:05 +0600

    updated for render site hosting

--- web_service/app.py
+++ web_service/app.py
@@ -5,11 +5,8 @@ import redis
 
 app = Flask(__name__)
 # Initialize Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='red-cogop0u3e1ms73e6rnj0', port=6379, db=0)
 
-@app.route('/' , methods=['GET'])
-def index():
-    return jsonify({"Flask Web Service Running"})
 
 # endpoint to receive tasks via POST request
 @app.route('/tasks', methods=['POST'])
