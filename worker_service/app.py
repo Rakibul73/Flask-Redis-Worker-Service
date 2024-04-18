86097d1b72d6b11db480611157d3382f275a80cb
Rakibul73 <rakibul29185@yahoo.com>
2024-04-19 03:35:20 +0600

    updated worker for hosting in render

--- worker_service/app.py
+++ worker_service/app.py
@@ -1,12 +1,13 @@
 # worker_service.py
 import json
+import os
 import time
 from flask import Flask, jsonify
 import redis
 import threading
 
 app = Flask(__name__)
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.from_url(os.environ['REDIS_URL'])
 
 @app.route('/' , methods=['GET'])
 def index():
